c0296c796b7f0fe8f4af5a9ceccf2345a4d2c9ea
Rick van der Wal <rick.van.der.wal@alliander.com>
2024-07-05 17:16:19 +0200

    Configure backend url

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,47 +2,46 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {v4 as uuidv4} from 'uuid';
 import {UserService} from "./user.service";
-import {map, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {Room} from "./room";
+import {environment} from "../environment/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  URL = 'http://localhost:8080';
-
   constructor(private http: HttpClient, private userService: UserService) {
   }
 
   public createRoom(): Observable<string> {
     let roomId = uuidv4();
     let userId = this.userService.getUser();
-    return this.http.post(`${this.URL}/rooms/${roomId}`, userId, {responseType: 'text'});
+    return this.http.post(`${environment.backendUrl}/rooms/${roomId}`, userId, {responseType: 'text'});
   }
 
   public registerUser(roomId: string): Observable<string> {
     let userId = this.userService.getUser();
-    return this.http.post(`${this.URL}/rooms/${roomId}/users/${userId}`, '', {responseType: 'text'});
+    return this.http.post(`${environment.backendUrl}/rooms/${roomId}/users/${userId}`, '', {responseType: 'text'});
   }
 
   public getRoom(roomId: string): Observable<Room> {
-    return this.http.get<Room>(`${this.URL}/rooms/${roomId}`);
+    return this.http.get<Room>(`${environment.backendUrl}/rooms/${roomId}`);
   }
 
   public reveal(roomId: string): Observable<string> {
-    return this.http.post(`${this.URL}/rooms/${roomId}/reveal`, '', {responseType: 'text'});
+    return this.http.post(`${environment.backendUrl}/rooms/${roomId}/reveal`, '', {responseType: 'text'});
   }
 
   public conceal(roomId: string): Observable<string> {
-    return this.http.post(`${this.URL}/rooms/${roomId}/conceal`, '', {responseType: 'text'});
+    return this.http.post(`${environment.backendUrl}/rooms/${roomId}/conceal`, '', {responseType: 'text'});
   }
 
   public submitCard(roomId: string, card: number): Observable<string> {
     let userId = this.userService.getUser();
-    return this.http.put(`${this.URL}/rooms/${roomId}/users/${userId}`, `${card}`, {responseType: 'text'});
+    return this.http.put(`${environment.backendUrl}/rooms/${roomId}/users/${userId}`, `${card}`, {responseType: 'text'});
   }
 
   public reset(roomId: string): Observable<string> {
-    return this.http.post(`${this.URL}/rooms/${roomId}/reset`, '', {responseType: 'text'});
+    return this.http.post(`${environment.backendUrl}/rooms/${roomId}/reset`, '', {responseType: 'text'});
   }
 }
---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  backendUrl: 'https://poker.tortoaster.com/api',
+};
